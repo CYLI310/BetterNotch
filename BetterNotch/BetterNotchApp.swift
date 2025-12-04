80158d761ba9a2cd3c8f2055da25ddc20eac89a8
CYLI310 <chengyangli3041@gmail.com>
2025-12-04 22:24:43 +0800

    Initial Commit

---
+++ BetterNotch/BetterNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterNotchApp.swift
+//  BetterNotch
+//
+//  Created by Justin Li on 2025/12/4.
+//
+
+import SwiftUI
+
+@main
+struct BetterNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterNotch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterNotch
+//
+//  Created by Justin Li on 2025/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
